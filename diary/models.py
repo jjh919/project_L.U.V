8c3eba6ecc4bfeb725a657ab4bff5f898eda46fe
jjh <wlsgur222@naver.com>
2023-05-29 22:01:55 +0900

    백업

--- diary/models.py
+++ diary/models.py
@@ -11,11 +11,6 @@ class Feed(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='feeds', 
                              null=True, blank=True, to_field='user_id')
 
-    def save(self, *args, **kwargs):
-        if not self.user_id and self.email:
-            self.user, _ = User.objects.get_or_create(email=self.email)
-        super().save(*args, **kwargs)
+    
 
-    def __str__(self):
-        print("에러")
-        return self.user.user_id
+
